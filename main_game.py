9b66614acb0c395c5e17fa5ecfb9ba93f3c49638
sivan <sivanb86@gmail.com>
2025-11-09 20:43:04 +0200

    hello new player

--- main_game.py
+++ main_game.py
@@ -1,2 +1,30 @@
 "welcome to the hangman game"
 
+import random
+
+print("Welcome to the Hangman game!")
+
+list_of_words = [
+    "banana",
+    "monkey",
+    "zoom",
+    "cat",
+    "dog",
+    "avocado",
+    "epic",
+    "watermelon",
+    "egg"
+]
+
+def choose_a_random_word(word_list: list):
+    len_of_list = len(word_list)
+    random_index = random.randint(a=0, b=len_of_list - 1)
+    return word_list[random_index]
+
+
+def print_hidden_word(word):
+    ...
+
+
+if __name__ == '__main__':
+    print(choose_a_random_word(list_of_words))
